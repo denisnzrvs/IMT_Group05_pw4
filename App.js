87f8d0c844fca5ce43564abe4dcaa6044bf9cb0c
lidziya Kamynina <lidziya.kamynina@va.lv>
2022-01-11 11:58:59 +0200

    Lidziya's application

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://chroniclingamerica.loc.gov/search/titles/results/?terms=election&format=json&limit=50&page=2')
+      .then((response) => response.json())
+      .then((json) => setData(json.items))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#ccc", padding:10}}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <View style={{marginTop: 5, backgroundColor: "#fff", padding: 14}}>
+              <Text style={{ fontSize: 16, fontWeight: "bold" }}>{item.title}, {item.country}</Text>
+              <Text>{item.alt_title}</Text>
+              <Text>{item.note}</Text>
+            </View>
+          )}
+        />
+      )}
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
